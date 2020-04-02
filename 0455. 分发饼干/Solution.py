0ce8cf07bc1adc21d8ba2751ddba86bf25e12313
decoin <jayzh1993@gmail.com>
2020-04-02 14:45:49 +0800

    Create Solution.py

---
+++ 0455. 分发饼干/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        counts = i = j = 0
+        g.sort()
+        s.sort()
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                counts += 1
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return counts
